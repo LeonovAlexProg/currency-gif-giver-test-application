e02c19cb47d45210f6b30007c146ca57fa942bb6
LeonovAlexProg <leonovalexprog@gmail.com>
2022-06-05 17:06:15 +0300

    Implemented main logic of giving Gif based on currency exchange rates.

--- src/main/java/com/personal/currencygifgiver/CurrencyExchangeGifController.java
+++ src/main/java/com/personal/currencygifgiver/CurrencyExchangeGifController.java
@@ -3,10 +3,21 @@ package com.personal.currencygifgiver;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 //комбинация аннотаций @Controller и @ResponseBody
 @RestController
@@ -21,12 +32,23 @@ public class CurrencyExchangeGifController {
     private final ApiClientCurrency apiClientCurrency;
     private final ApiClientGif apiClientGif;
 
+
+
     @GetMapping ("/")         //@GetMapping — Обрабатывает get-запросы
-    public ResponseEntity readCurrencyExchangeData () {
-        if (apiClientCurrency.getCurrency().getRates().getRub() >
+    public ResponseEntity readCurrencyExchangeData () throws URISyntaxException {
+        if (apiClientCurrency.getCurrency().getRates().getRub() <
                 apiClientCurrency.getYesterdayCurrency().getRates().getRub()) {
-            return ResponseEntity.ok(apiClientGif.getRichGifs().getData().getImages().getOriginal().getUrl());
-        } else
-            return ResponseEntity.ok(apiClientGif.getBrokeGifs().getData().getImages().getOriginal().getUrl());
+            URI externalUri = new URI(apiClientGif.getRichGifs()
+                    .getData().getImages().getOriginal().getUrl());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(externalUri);
+            return new ResponseEntity(httpHeaders, HttpStatus.SEE_OTHER);
+        } else {
+            URI externalUri = new URI(apiClientGif.getBrokeGifs()
+                    .getData().getImages().getOriginal().getUrl());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(externalUri);
+            return new ResponseEntity(httpHeaders, HttpStatus.SEE_OTHER);
+        }
 }
 }
